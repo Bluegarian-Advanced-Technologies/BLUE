47a3f29812eabfeb280f5228bf422f35660c927d
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-10-11 11:39:16 -0400

    feat: shuffle queue

---
+++ commands/music/shuffle.js
@@ -0,0 +1,21 @@
+module.exports = {
+  id: "shuffle",
+  description: "Shuffles the queue",
+  category: "Music",
+  aliases: [],
+  slash: true,
+  permissions: ["Speak", "Connect"],
+  expectedArgs: [],
+  async execute(command, { client, guildId, member, embedReply }) {
+    const vc = member.voice?.channel?.id;
+    if (vc == null) return await embedReply("Not connected", "You must be connected to a voice channel to use this command.", "error");
+
+    const player = client.audioManager.players.get(guildId);
+    if (!player) return await embedReply("Not connected", "The bot is not connected to the voice channel.", "error");
+    if (player.voiceChannel !== vc)
+      return await embedReply("Not connected", "You must be connected to the same voice channel as the bot to use this command.", "error");
+
+    player.queue.shuffle();
+    await embedReply("🔀 Shuffled the queue");
+  },
+};
